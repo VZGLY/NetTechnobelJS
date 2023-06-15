9f4fca45013ba91a9a42f64f89f75ed97ca5ab6f
VZGLY <113683718+BS-GavinC@users.noreply.github.com>
2023-06-15 14:41:36 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+
+
+
+let addItem = () => {
+    let list = document.getElementById("maList")
+    let newElem = document.createElement("li")
+    newElem.innerHTML = "Item."
+    list.appendChild(newElem)
+}
+
